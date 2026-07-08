9bb782ba31920d22b21d18345e4317e99e22fe29
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-08 15:49:08 +0530

    Update layers.py

--- MAMLsr/models/layers.py
+++ MAMLsr/models/layers.py
@@ -1,1 +1,133 @@
+import torch
+import torch.nn as nn
 
+
+class ConvBlock(nn.Module):
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        stride=2,
+        padding=1
+    ):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class DeconvBlock(nn.Module):
+
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=4,
+        stride=2,
+        padding=1
+    ):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class Flatten(nn.Module):
+
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class UnFlatten(nn.Module):
+
+    def __init__(self, channels, height, width):
+        super().__init__()
+
+        self.channels = channels
+        self.height = height
+        self.width = width
+
+    def forward(self, x):
+
+        return x.view(
+            x.size(0),
+            self.channels,
+            self.height,
+            self.width
+        )
+
+
+class ResidualBlock(nn.Module):
+
+    def __init__(self, channels):
+        super().__init__()
+
+        self.block = nn.Sequential(
+
+            nn.Conv2d(
+                channels,
+                channels,
+                kernel_size=3,
+                padding=1,
+                bias=False
+            ),
+
+            nn.BatchNorm2d(channels),
+
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(
+                channels,
+                channels,
+                kernel_size=3,
+                padding=1,
+                bias=False
+            ),
+
+            nn.BatchNorm2d(channels)
+
+        )
+
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+
+        identity = x
+
+        out = self.block(x)
+
+        out += identity
+
+        out = self.relu(out)
+
+        return out
