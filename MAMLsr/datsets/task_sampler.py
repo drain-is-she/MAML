8f2dde0f9f75634f684d4bc69b9dc21d38bc15af
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-08 15:45:26 +0530

    Implement TaskSampler for sampling tasks from dataset

--- MAMLsr/datsets/task_sampler.py
+++ MAMLsr/datsets/task_sampler.py
@@ -1,1 +1,46 @@
+import random
+import torch
 
+
+class TaskSampler:
+
+    def __init__(
+        self,
+        dataset,
+        support_size,
+        query_size
+    ):
+
+        self.dataset = dataset
+        self.support_size = support_size
+        self.query_size = query_size
+
+    def sample_task(self):
+
+        total = self.support_size + self.query_size
+
+        
+        indices = random.sample(
+            range(len(self.dataset)),
+            total
+        )
+
+        support_indices = indices[:self.support_size]
+        query_indices = indices[self.support_size:]
+
+        support_images = []
+        query_images = []
+
+      
+        for idx in support_indices:
+            image = self.dataset[idx]
+            support_images.append(image)
+
+        for idx in query_indices:
+            image = self.dataset[idx]
+            query_images.append(image)
+
+        support_images = torch.stack(support_images)
+        query_images = torch.stack(query_images)
+
+        return support_images, query_images
