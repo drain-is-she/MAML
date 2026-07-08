38099e1e671987debe00930cfcafc22c3f65dc81
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-08 15:44:40 +0530

    Add SRDataset class for image dataset handling
    Implemented SRDataset class for loading low and high resolution images.

--- MAMLsr/datsets/dataset.py
+++ MAMLsr/datsets/dataset.py
@@ -1,1 +1,26 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class SRDataset(Dataset):
+    def __init__(self, lr_dir, hr_dir, transform=None):
+        self.lr_dir = lr_dir
+        self.hr_dir = hr_dir
+        self.transform = transform
+        self.image_names = sorted(os.listdir(lr_dir))
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+        filename = self.image_names[idx]
+        lr = Image.open(os.path.join(self.lr_dir, filename)).convert("RGB")
+        hr = Image.open(os.path.join(self.hr_dir, filename)).convert("RGB")
+
+        if self.transform:
+            lr = self.transform(lr)
+            hr = self.transform(hr)
+
+        return lr, hr
 
