34b679b95b47e0bd8b8dd448ba134f84960fcac5
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-08 15:48:51 +0530

    Update encoder.py

--- MAMLsr/models/encoder.py
+++ MAMLsr/models/encoder.py
@@ -1,1 +1,24 @@
+import torch 
+import torch.nn as nn 
+from .layers import ConvBlock , Flatten 
 
+class Encoder(nn.Module):
+    def __init__(self,image_channels = 3 , latent_dim = 128 ):
+        super().__init__()
+        self.features = nn.Sequential(
+            ConvBlock(image_channels,32),
+            ConvBlock(32,64),
+            ConvBlock(64,128),
+            ConvBlock(128,256),
+        )
+        self.flatten = Flatten()
+        self.feature_dim = 256*8*8
+        self.fc_mu = nn.Linear(self.feature_dim , latent_dim)
+        self.fc_logvar = nn.Linear(self.feature_dim,latent_dim )
+
+    def forward(self,x):
+        x = self.features(x)
+        x = self.flatten(x)
+        mu = self.fc_mu(x)
+        log_var = self.fc_logvar(x)
+        return mu , log_var
