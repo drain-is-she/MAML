762aaca008e776ecff81952dabfbc354139ab13b
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-08 15:46:34 +0530

    Update transforms.py

--- MAMLsr/datsets/transforms.py
+++ MAMLsr/datsets/transforms.py
@@ -1,1 +1,8 @@
+from torchvision import transforms
 
+
+def get_transform(image_size=128):
+    return transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+    ])
