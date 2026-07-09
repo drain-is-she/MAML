8b21bc672340ccc145fb35ecc696186d203eaebc
drain-is-she <priyanshi.code2412@gmail.com>
2026-07-09 16:17:22 +0530

    Add ClassificationLoss class with label smoothing

--- classifier/losses.py
+++ classifier/losses.py
@@ -1,1 +1,14 @@
+import torch.nn as nn
 
+
+class ClassificationLoss(nn.Module):
+    def __init__(self, label_smoothing=0.1):
+        super().__init__()
+
+        self.criterion = nn.CrossEntropyLoss(
+            label_smoothing=label_smoothing
+        )
+
+    def forward(self, logits, labels):
+        loss = self.criterion(logits, labels)
+        return loss
